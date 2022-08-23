fed6c9a38ff2c9151ccfa8712ff67cd3bcc281ba
xavy92 <javaz_15@hotmail.com>
2022-08-23 00:03:23 -0500

    imagenes y boton

---
+++ code.js
@@ -0,0 +1,23 @@
+//Agregar el evento clic al boton
+let btnIniciar = document.querySelector(".inicio")
+
+btnIniciar.addEventListener("click", () => {
+    console.log("inicia el juegooooo")
+})
+
+//seleccionar canvas 
+let lienzo = document.getElementById("lienzo")
+//Acceso al metodo getContext()
+
+let ctx = lienzo.getContext("2d")
+
+//dibujar linea
+
+function dibujar piso 
+
+function iniciarJuego () {
+
+}
+
+
+
